91105663fef261e4d88537cf057a60c96c2e4cb2
narie <monarijob@yahoo.com>
2022-07-14 03:05:19 +0300

    changes

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module narie-monarie/go-site
 
 go 1.18
+
+require github.com/go-chi/chi/v5 v5.0.7
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -13,22 +15,32 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Welcome to my Contact site man</h1>")
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Welcome to my Contact site man</h1>")
+}
+
 //Raw path - used mostly in emails because its url is ?key=value
 func pathHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
-	case "/contact":
+	case "/faq":
 		contactHandler(w, r)
 	default:
 		//TODO: handle page not found
 		http.Error(w, "Page not Found", http.StatusNotFound)
 	}
 }
-
 func main() {
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not Found", http.StatusNotFound)
+	})
 	//http.HandleFunc("/", pathHandler)
 	//http.HandleFunc("/contact", contactHandler)
 	fmt.Println("listening on localhost 3000...")
-	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
+	http.ListenAndServe(":3000", r)
 }
